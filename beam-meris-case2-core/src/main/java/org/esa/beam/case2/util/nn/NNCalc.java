7dce2c80549142538d785edbbab9abc815707401
Marco Peters <marco.peters@brockmann-consult.de>
2010-03-31 10:53:56 +0200

    push

--- beam-meris-case2-core/src/main/java/org/esa/beam/case2/util/nn/NNCalc.java
+++ beam-meris-case2-core/src/main/java/org/esa/beam/case2/util/nn/NNCalc.java
@@ -3,7 +3,7 @@ package org.esa.beam.case2.util.nn;
 /**
  * This class serves for defining a structure containing the output of a NN together
  * with the corresponding Jacobi matrix. An instance of this class is returned by the
- * {@link org.esa.beam.lakes.eutrophic.util.nn.NNffbpAlphaTabFast#calcJacobi(double[])} method.
+ * {@link org.esa.beam.case2.util.nn.NNffbpAlphaTabFast#calcJacobi(double[])} method.
  *
  * @author K. Schiller
  *         Copyright GKSS/KOF
@@ -11,7 +11,7 @@ package org.esa.beam.case2.util.nn;
  */
 public class NNCalc {
     /**
-     * The vector containing the output of a {@link org.esa.beam.lakes.eutrophic.util.nn.NNffbpAlphaTabFast}.
+     * The vector containing the output of a {@link org.esa.beam.case2.util.nn.NNffbpAlphaTabFast}.
      */
     public double[] nnOutput;
     /**
