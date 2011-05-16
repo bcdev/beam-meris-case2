b1853e31e310b583d023d421dc6f2cac5f955aba
marpet <marco.peters@brockmann-consult.de>
2011-05-16 12:24:48 +0200

    renamed module to 'beam-meris-case2-regional'

--- src/main/resources/doc/help/merisCase2Regional.hs
+++ src/main/resources/doc/help/merisCase2Regional.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>Meris Case-2 IOP Help</title>
+    <title>Meris Case-2 Regional Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.xml"/>
