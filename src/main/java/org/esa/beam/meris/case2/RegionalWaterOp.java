41b69909131ecb26948fe7aee15e99126f6c3a54
Olaf Danne <olaf.danne@brockmann-consult.de>
2013-04-23 13:55:15 +0200

    set correct processor version in metadata

--- src/main/java/org/esa/beam/meris/case2/RegionalWaterOp.java
+++ src/main/java/org/esa/beam/meris/case2/RegionalWaterOp.java
@@ -50,7 +50,7 @@ import static org.esa.beam.meris.case2.water.WaterAlgorithm.*;
                   description = "Performs IOP retrieval on atmospherically corrected MERIS products.",
                   authors = "Roland Doerffer (GKSS); Marco Peters (Brockmann Consult)",
                   copyright = "(c) 2010 by Brockmann Consult",
-                  version = "1.7-CC",
+                  version = "1.8.3-CC-SNAPSHOT",
                   internal = true)
 public class RegionalWaterOp extends PixelOperator {
 
